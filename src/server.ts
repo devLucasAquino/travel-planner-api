e9435139b559a5043971478bd325df16a93c1c79
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-07-13 15:24:33 -0300

    feat: adaptando cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import fastify from "fastify";
 import { createTrip } from './routes/create-trip';
 import { confirmTrip } from './routes/confirm-trip';
+import cors from '@fastify/cors';
 import { validatorCompiler, serializerCompiler } from 'fastify-type-provider-zod';
 
 
 const app = fastify();
+
+app.register(cors, {
+    origin: '*',
+})
 
 app.setValidatorCompiler( validatorCompiler );
 app.setSerializerCompiler( serializerCompiler );
