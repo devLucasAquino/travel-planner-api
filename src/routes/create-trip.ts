2e2f10edfce4f3a315142dc4415f782586f6b404
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-07-13 14:38:47 -0300

    feat: cadastrando emails e nomes de convidados

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -15,10 +15,11 @@ export async function createTrip(app: FastifyInstance){
                 ends_at: z.coerce.date(),
                 owner_name: z.string(),
                 owner_email: z.string().email(),
+                emails_to_invite: z.array(z.string().email()),
             })
         }
     }, async (request) => {
-        const { destination, starts_at, ends_at, owner_name, owner_email } = request.body;
+        const { destination, starts_at, ends_at, owner_name, owner_email, emails_to_invite } = request.body;
 
         if(dayjs(starts_at).isBefore(new Date())){
             throw new Error('invalid trip start date.');
@@ -28,11 +29,27 @@ export async function createTrip(app: FastifyInstance){
             throw new Error('invalid trip end date.');
         }
 
+
         const trip = await prisma.trip.create({
             data: {
                 destination,
                 starts_at,
                 ends_at,
+                pasticipants: {
+                    createMany: {
+                        data: [
+                            {
+                                name: owner_name,
+                                email: owner_email,
+                                is_owner: true,
+                                is_confirmed: true,
+                            },
+                            ...emails_to_invite.map(email => {
+                                return {email}
+                            })
+                        ]
+                    }
+                }
             }
         })
 
