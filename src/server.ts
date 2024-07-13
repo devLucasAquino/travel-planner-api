4121d4d59ad211b88c7cbdcb087754a0b42730f0
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-07-13 12:11:18 -0300

    feat: config fastify

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,10 @@ import fastify from "fastify";
 
 const app = fastify();
 
+app.get('/teste', () => {
+    return 'Hello World!'
+})
+
 app.listen({port: 3333}).then(() => {
     console.log('server running!')
 })
