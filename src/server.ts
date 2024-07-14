0feefb4edeeeed2af1277e68403903210046d3c2
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-07-13 21:13:10 -0300

    feat: created getLinks routes

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,10 @@ import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod
 import { confirmParticipant } from './routes/confirm-participant';
 import { confirmTrip } from './routes/confirm-trip';
 import { createActivity } from './routes/create-activity';
+import { createLink } from './routes/create-link';
 import { createTrip } from './routes/create-trip';
 import { getActivities } from './routes/get-activities';
-import { createLink } from  './routes/create-link'; 
+import { getLinks } from './routes/get-links';
 
 
 const app = fastify();
@@ -24,6 +25,7 @@ app.register(confirmParticipant)
 app.register(createActivity)
 app.register(getActivities)
 app.register(createLink)
+app.register(getLinks)
 
 app.listen({port: 3333}).then(() => {
     console.log('server running!')
