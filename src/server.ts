badb7ca193580a9b3f7b8e8381a89c7bd16dcfd2
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-07-13 22:09:05 -0300

    feat: get participant

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,7 @@ import { createLink } from './routes/create-link';
 import { createTrip } from './routes/create-trip';
 import { getActivities } from './routes/get-activities';
 import { getLinks } from './routes/get-links';
+import { getParticipant } from './routes/get-participant';
 import { getParticipants } from './routes/get-participants';
 import { getTripDetails } from './routes/get-trip-details';
 import { updateTrip } from './routes/update-trip';
@@ -34,6 +35,7 @@ app.register(getParticipants)
 app.register(createInvite)
 app.register(updateTrip)
 app.register(getTripDetails)
+app.register(getParticipant)
 
 app.listen({port: 3333}).then(() => {
     console.log('server running!')
